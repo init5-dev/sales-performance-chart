e1d3d28536c8c3567ee0571c6273622b9a3bc7b4
Nelson Ochagavía <nelson.ochagavia@gmail.com>
2023-12-20 20:54:30 -0500

    Vite & express server configured

---
+++ src/server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+const SERVER_PORT = 3000
+
+app.set('port', SERVER_PORT);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
