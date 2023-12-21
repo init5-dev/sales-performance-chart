d50b911b2ef8fdef01ac890f420d7f97791d654d
Nelson Ochagavía <nelson.ochagavia@gmail.com>
2023-12-21 16:00:48 -0500

    Updated comments

--- src/server/db/connection.mjs
+++ src/server/db/connection.mjs
@@ -1,38 +1,41 @@
-
+// Import necessary modules from dependencies
 import dotenv from 'dotenv'
 import process from "process"
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
+// Load environment variables from a .env file
 dotenv.config()
 
+// Extract MongoDB username and password from environment variables
 const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 
+// Construct MongoDB connection URI with username and password
 const URI = `mongodb+srv://${username}:${password}@salesdb.xcpwkzp.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
+// Create a MongoClient with specified options, including setting the Stable API version
 const client = new MongoClient(URI, {
   serverApi: {
-    version: ServerApiVersion.v1,
+    version: ServerApiVersion.v1, // Set the Stable API version
     strict: true,
     deprecationErrors: true,
   }
 });
 
-// Establish connection
-
+// Initialize variable to hold the connection
 let conn;
 
+// Try to establish a connection to MongoDB
 try {
-  conn = await client.connect();
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  conn = await client.connect(); // Connect to the MongoDB server
+  await client.db("admin").command({ ping: 1 }); // Ping the server to check the connection
+  console.log("Connected to MongoDB database!");
 } catch (e) {
-  console.error(e);
+  console.error(e); // Log an error if connection fails
 }
 
+// Access the "sales_db" database from the established connection
 const db = conn.db("sales_db");
-console.log(db)
 
+// Export the connected MongoDB database for use in other parts of the application
 export default db;
